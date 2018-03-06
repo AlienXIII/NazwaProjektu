4df3a86d83286e15f625e7a7ccb9192b6050293e
JR <j.robocinski@gmail.com>
2018-03-06 11:11:56 +0100

    trzeci jako jr

--- src/Client.java
+++ src/Client.java
@@ -2,4 +2,14 @@
  * Created by hp on 05.03.2018.
  */
 public class Client {
+
+    private String poprzedniZakup;
+
+    public String poprzedniZakup(){
+        return poprzedniZakup;
+    }
+
+    public void setpoprzedniZakup(String poprzedniZakup) {
+        this.poprzedniZakup = poprzedniZakup;
+    }
 }
