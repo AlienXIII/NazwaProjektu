962aa61d7aafb1583c9a1084f2ffdfc351e6c69f
Maciek <maciej.jurowski@gmail.com>
2018-03-06 11:04:21 +0100

    second commit

--- src/Employee.java
+++ src/Employee.java
@@ -14,4 +14,13 @@ public class Employee extends Osoba{
     public void setDzial(String dzial) {
         this.dzial = dzial;
     }
+    private int payroll;
+
+    public int getpayroll(){
+        return payroll;
+    }
+
+    public void setpayroll(int payroll) {
+        this.payroll = payroll;
+    }
 }
